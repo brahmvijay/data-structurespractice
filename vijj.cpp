4b708d7ddd7ae2b7234f98d1fb55d88776d504e5
Brahm vijay <brahmvijay2002@gmail.com>
2022-06-04 16:45:06 +0530

    program of printing an array is made

---
+++ vijj.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ using namespace std; 
+ enum day {mon = 1 , tues , wed , sat=8};
+int main(){
+day d ;
+day m ;
+d= mon;
+m = wed ;
+cout<<m<<endl;
+cout<<d;
+
+return 0;
+
+}
