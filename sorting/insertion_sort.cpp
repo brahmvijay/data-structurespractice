00a86b593944b2fc335d4d4720dafcebece0da28
Brahm vijay <brahmvijay2002@gmail.com>
2022-06-13 03:23:52 +0530

    all the files are arranged in a proper order

---
+++ sorting/insertion_sort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ using namespace std; 
+void insertion_sorting(){
+  for(int i =1;i<n-1;i++){
+    for(int j = 0;j<=n-1;j++){
+        
+    }
+
+  }
+}
+int main(){
+int arr[] = {2,5,3,9,4};
+int n = sizeof(arr)/sizeof(int);
+
+insertion_sorting(arr,n);
+for(int i =0;i<n-1;i++){
+    cout<<arr[i]<<",";
+}
+return 0;
+
+}
